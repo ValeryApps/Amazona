eebe218ae3cb825698b7704aa2693563f6e32d77
Valery <valeryghn10@outlook.com>
2022-06-12 12:41:53 +0000

    First commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div >
+      <header >
+        <a href="/">Amazona</a>
+      </header>
+      <main>
+        <h2>List of products</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
